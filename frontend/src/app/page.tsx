fbbdc159664f27983f1058a6acacd03b93fc91d0
Ryoma <ryoma@40-60studio.com>
2024-06-05 16:28:06 +0200

    chore: Update Home page layout and add join form

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,8 @@ import { PartyPreview } from "@/components/GamePreview";
 import { PartyContext } from "@/context/game-context";
 
 import { useContext } from "react";
-import Image from "next/image";
+import { Button } from "@/components/button";
+import { Input } from "postcss";
 
 export default function Home() {
   const { parties } = useContext(PartyContext);
@@ -28,6 +29,16 @@ export default function Home() {
         />
       </div>
       <button className="p-4 rounded bg-orange-400">Add game</button>
+      <div className="flex flex-col items-center justify-center max-w-3xl">
+        <h1 className="text-6xl text-white text-center mb-16">Joindre Les Ptits Loups</h1>
+        <div className="w-full max-w-sm flex flex-col">
+          <label htmlFor="pseudo" className="text-white text-lg">
+            Pseudo
+          </label>
+          <Input className="mb-8" placeholder="John Doe"/>
+          <Button onClick={handleCreateGame}>Joindre</Button>
+        </div>
+      </div>
     </main>
   );
 }
