27739c7449bfb8c5efc89d4fb0ee2cb86e5a7222
Ryoma <ryoma@40-60studio.com>
2024-06-05 14:48:05 +0200

    new button

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main>page</main>;
+  return (
+    <main>
+      <button className="p-4 rounded bg-orange-300">Add game</button>
+    </main>
+  );
 }
