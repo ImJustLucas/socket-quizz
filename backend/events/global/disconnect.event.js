a02aecd902d6e496b45ba14a2d9107cb149b9b23
ImJustLucas <lucas.bellier@icloud.com>
2024-06-06 10:11:29 +0200

    chore: Clear disconnected user from parties on socket disconnect

--- backend/events/global/disconnect.event.js
+++ backend/events/global/disconnect.event.js
@@ -1,4 +1,8 @@
+import partyManagementEvents from "../party-culture/party-management.events.js";
+
 export const socketOnDisconnet = (socket) => () => {
   console.log("user disconnected");
   socket.broadcast.emit("user disconnected");
+
+  partyManagementEvents.clearDisconnectedUser(socket)();
 };
--- backend/events/party-culture/party-management.events.js
+++ backend/events/party-culture/party-management.events.js
@@ -6,6 +6,8 @@ const updatePartiesList = (socket) =>
   });
 
 const updateParty = (socket, io) => (partyId) => {
+  console.log("@Update party", partyId);
+
   if (!parties[partyId]) {
     return socket.emit("party-not-found");
   }
@@ -13,9 +15,24 @@ const updateParty = (socket, io) => (partyId) => {
   io.to(partyId).emit("party-update-one", parties[partyId]);
 };
 
+const clearDisconnectedUser = (socket, io) => () => {
+  for (const partyId in parties) {
+    const party = parties[partyId];
+    const index = party.members.indexOf(socket.id);
+
+    if (index !== -1) {
+      party.members.splice(index, 1);
+      io.to(partyId).emit("party-update-one", party);
+    }
+  }
+  updatePartiesList(socket);
+  console.log("@Clear user from parties");
+};
+
 const partyManagementEvents = {
   updatePartiesList,
   updateParty,
+  clearDisconnectedUser,
 };
 
 export default partyManagementEvents;
